4f429b1260cb0151ba12565c96502b9b05401cc3
전성민 <@DESKTOP-8V3Q01O>
2022-08-30 19:35:58 +0900

    8.2. 변수의 효용

---
+++ java1/Data_and_operation/Letter.java
@@ -0,0 +1,12 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "jeon";
+		System.out.println("Hello, "+name+" ... "+name+" ... jeon ... bye");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+
+}
